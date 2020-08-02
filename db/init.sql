d4d1bf05b39e7bd7d542ed3720313aef3e85d607
kb8njit <kb8@njit.edu>
2020-08-02 18:44:16 -0400

    Task: Continuing to work on signin and registration

--- db/init.sql
+++ db/init.sql
@@ -5,14 +5,14 @@ DROP TABLE if exists Accounts;
 
 CREATE TABLE IF NOT EXISTS Accounts (
     `id` INT AUTO_INCREMENT,
-    `Username` VARCHAR(33) NOT NULL,
-    `Password` VARCHAR(33) NOT NULL,
     `First_Name` VARCHAR(60) NOT NULL,
     `Last_Name` VARCHAR(60) NOT NULL,
     `Email` VARCHAR(100) NOT NULL,
+    `Password` VARCHAR(33) NOT NULL,
+    `Verified` INT,
     PRIMARY KEY (`id`)
 );
-INSERT INTO Accounts (Username, Password, First_Name, Last_Name, Email) VALUES
-    ('test', 'testpw', 'testfn', 'testln', 'testem@test.com'),
-    ('KBlackledge', 'Password', 'Kelly', 'Blackledge', 'kb8@njit.edu');
+INSERT INTO Accounts (First_Name, Last_Name, Email, Password, Verified) VALUES
+    ('FN', 'LN', 'EM@gmail.com', 'PW', '1'),
+    ('Kelly', 'Blackledge', 'kb8@njit.edu', 'Password', '1');
 
